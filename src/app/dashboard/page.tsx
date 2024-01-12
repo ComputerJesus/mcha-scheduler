88f3a9e79c7c41096f5a8069b6be3e3e234ce08e
SHSFootballRepo <ggr697385@gnspes.ca>
2024-01-12 16:46:47 -0400

    Add Beginings of Calender, fix middleware and alter git ignore

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import { currentUser, UserButton } from '@clerk/nextjs';
 import { redirect } from 'next/navigation'
-import header from '@/componants/header';
+import React from 'react';
+
 
 export default async function auth() {
+
+
   const user = await currentUser();
   console.log(user?.firstName + " " + user?.lastName + " Tried to access dashboard, Got : " + user?.publicMetadata.isAllowed);
 
@@ -14,9 +17,22 @@ export default async function auth() {
   return(
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       
+      
 
       <h2 className='font-semibold text-3xl mb-4 p' >Hello {user?.firstName}!</h2>
       <h1 className='font-semibold mb-4'>Welcome to the Dashboard</h1>
+      <div>
+          <div className="grid grid-cols-7 gap-2 bg-gray-200 p-2">
+            <p>Monday</p>
+            <p>Tuesday</p>
+            <p>Wednesday</p>
+            <p>Thursday</p>
+            <p>Friday</p>
+            <p>Saturday</p>
+            <p>Sunday</p>
+          </div>
+      </div>
+
       
     </main>
     
