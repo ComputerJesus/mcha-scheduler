7605177869506e0df6f9ce546d1b6e14799eb5d8
SHSFootballRepo <ggr697385@gnspes.ca>
2024-01-12 16:56:39 -0400

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from '@vercel/analytics/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}
       <SpeedInsights />
+      <Analytics />
       </body>
     </html>
     </ClerkProvider>
